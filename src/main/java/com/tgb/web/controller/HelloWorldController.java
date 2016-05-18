838e6c126e9e04b0a719d46ce9035822a17fd70e
Rayest <yi@rayest.cn>
2016-05-18 12:18:34 +0800

    Added: return String and return ModelAndView with value-passing

--- src/main/java/com/tgb/web/controller/HelloWorldController.java
+++ src/main/java/com/tgb/web/controller/HelloWorldController.java
@@ -1,17 +1,43 @@
 package com.tgb.web.controller;
 
+import org.omg.CORBA.Object;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/5/16 0016.
  */
-@Controller
+/*@Controller
 public class HelloWorldController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(){
+    public String index(Map<String, String> model){
         System.out.println("I am working");
+        model.put("name","ray");
+        model.put("age","24");
+        model.put("sex","male");
         return "welcome";
     }
+}*/
+@Controller
+public class HelloWorldController {
+    @RequestMapping
+    public ModelAndView handleRequest(HttpServletRequest request,
+                                      HttpServletResponse response) throws Exception {
+        Map information = new HashMap();
+        information.put("name", "ray");
+        information.put("age","24");
+        information.put("sex","male");
+        return new ModelAndView("welcome", "map", information);
+    }
+
 }
+
+
