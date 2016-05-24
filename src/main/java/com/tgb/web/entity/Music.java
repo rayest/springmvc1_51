7b48545d164e44b7356f710e60c9f1ec7b423059
Rayest <yi@rayest.cn>
2016-05-24 21:41:56 +0800

    Classify some examples

---
+++ src/main/java/com/tgb/web/entity/Music.java
@@ -0,0 +1,28 @@
+package com.tgb.web.entity;
+
+/**
+ * Created by Administrator on 2016/5/23 0023.
+ */
+public class Music {
+    private String name = "Remember When";
+    private int year = 2011;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
+
+
+
